14c839e0c4d8db01e9a3729ca9592461fd1b07d4
Vania2007 <momotis2007@gmail.com>
2024-10-23 18:09:38 +0300

    rebuilding 3.4 ex.

--- 3.4.php
+++ 3.4.php
@@ -1,7 +1,15 @@
 <?php
-$text = "When I go to the store, I always buy apples because apples are very tasty and I like apples more than other fruits.";
+function getUniqueWords($text) {
+    $words = preg_split('/[\s,.;\-?!]+/', $text);
+    $unique_words = array_filter(array_unique($words), function($word) {
+        return $word !== '';
+    });
+    return $unique_words;
+}
 
-$words = preg_split('/\s+/', $text);
-$unique_words = array_unique($words);
+$text = "When I go to the store, I always buy apples because apples are very tasty and I like apples more than other fruits.";
+$unique_words = getUniqueWords($text);
 
 print_r($unique_words);
+?>
+
