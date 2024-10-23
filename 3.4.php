ac950f4daf025cb92138b378f49abb6e71453db5
Vania2007 <momotis2007@gmail.com>
2024-10-23 18:13:19 +0300

    change name of changeable

--- 3.4.php
+++ 3.4.php
@@ -1,15 +1,14 @@
 <?php
-function getUniqueWords($text) {
+function getUniqueWords($text)
+{
     $words = preg_split('/[\s,.;\-?!]+/', $text);
-    $unique_words = array_filter(array_unique($words), function($word) {
+    $unique_words = array_filter(array_unique($words), function ($word) {
         return $word !== '';
     });
     return $unique_words;
 }
 
-$text = "When I go to the store, I always buy apples because apples are very tasty and I like apples more than other fruits.";
-$unique_words = getUniqueWords($text);
+$str = "When I go to the store, I always buy apples because apples are very tasty and I like apples more than other fruits.";
+$unique_words = getUniqueWords($str);
 
 print_r($unique_words);
-?>
-
